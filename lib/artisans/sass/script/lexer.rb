2be2f79a6817f16ceb813bc3aa2be5671b6b0041
Myroslava Stavnycha <myroslava@perfectline.co>
2017-08-22 13:28:59 +0300

    parse rob with settings

--- lib/artisans/sass/script/lexer.rb
+++ lib/artisans/sass/script/lexer.rb
@@ -3,17 +3,18 @@
 # surrounded with settings comments
 #
 module Artisans
-  module SettingsColorLexer
+  module SettingsLexer
 
     def self.prepended(base)
       base.class_eval do
         class << self
           def settings_re(re)
-            Regexp::new("(\\'?/\\*settings\\.[a-z_]+\\[\\*/)?(" + re.source + ")(/\\*\\]\\*/\\'?)?")
+            Regexp::new("(\\'?/\\*settings\\.[a-z_]+\\[\\*/)(" + re.source + ")(/\\*\\]\\*/\\'?)")
           end
         end
 
-        self::REGULAR_EXPRESSIONS[:settings_color] = settings_re(self::HEXCOLOR)
+        self::REGULAR_EXPRESSIONS[:settings_color] = settings_re(self::REGULAR_EXPRESSIONS[:color])
+        self::REGULAR_EXPRESSIONS[:settings_rgb_color] = settings_re(/rgba?\((\d+)\s*,(\d+)\s*,(\d+)\s*,(\d+)?\s*\)/)
 
         private
 
@@ -35,7 +36,7 @@ module Artisans
           # Injecting 'settings_color'
           # lexer check in here
           #
-          variable || settings_color || string(:double, false) || string(:single, false) || color || number || id ||
+          variable || settings_color || settings_rgb_color || string(:double, false) || string(:single, false) || color || number || id ||
             selector || string(:uri, false) || raw(self.class::UNICODERANGE) || special_fun || special_val ||
             ident_op || ident || op
         end
@@ -52,9 +53,17 @@ module Artisans
           script_color.instance_variable_set("@representation", @scanner[0].gsub(/^'/, '').gsub(/'$/, ''))
           [:color, script_color]
         end
+
+        def settings_rgb_color
+          return unless @scanner.match?(self.class::REGULAR_EXPRESSIONS[:settings_rgb_color])
+          scanned_color = scan(self.class::REGULAR_EXPRESSIONS[:settings_rgb_color])
+          script_color = ::Sass::Script::Value::Color.new([@scanner[3].to_f, @scanner[4].to_f, @scanner[5].to_f, @scanner[6].to_f || 1])
+          script_color.instance_variable_set("@representation", @scanner[0].gsub(/^'/, '').gsub(/'$/, ''))
+          [:color, script_color]
+        end
       end
     end
   end
 end
 
-::Sass::Script::Lexer.send(:prepend, Artisans::SettingsColorLexer)
+::Sass::Script::Lexer.send(:prepend, Artisans::SettingsLexer)
